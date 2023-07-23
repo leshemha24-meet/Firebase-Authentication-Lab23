5210645c3308633b00069b3c8b04fad15e1e40e9
leshemha24-meet <leshem.hadad@gmail.com>
2023-07-23 16:25:22 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,18 +2,50 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyD4MzuEYxNJRkfVxYeJbVKNvVIPVrHvulU",
+  "authDomain": "project-8c897.firebaseapp.com",
+  "projectId": "project-8c897",
+  "storageBucket": "project-8c897.appspot.com",
+  "messagingSenderId": "808793693155",
+  "appId": "1:808793693155:web:c09a6a9a8935d406e61ed6",
+  "measurementId": "G-6W3YWE6DT2",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            return render_template("signin.html")
+    else:
+        return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+        except:
+            return render_template("signup.html")
+    else:
+        return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
