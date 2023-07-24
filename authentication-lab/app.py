1fa173ea91a38cb5452c8361def15017ed1a6c69
leshemha24-meet <leshem.hadad@gmail.com>
2023-07-24 12:15:34 +0300

    updated

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,11 +10,12 @@ config = {
   "messagingSenderId": "808793693155",
   "appId": "1:808793693155:web:c09a6a9a8935d406e61ed6",
   "measurementId": "G-6W3YWE6DT2",
-  "databaseURL": ""
+  "databaseURL": "https://project-8c897-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
@@ -39,8 +40,14 @@ def signup():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
+        fullname = request.form['fullname']
+        username = request.form['username']
+        bio = request.form['bio']
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID = login_session['user']['localId']
+            user = {'bio':bio, 'fullname':fullname, 'username':username}
+            db.child('Users').child(UID).set(user)
             return redirect(url_for('add_tweet'))
         except:
             return render_template("signup.html")
@@ -50,7 +57,14 @@ def signup():
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        tweet = {'title':title, 'text':text, 'uid':login_session['user']['localId']}
+        db.child('Users').child('UID').child('Tweets').set(tweet)
+        return redirect(url_for('add_tweet'))
+    else:
+        return render_template("tweet.html")
 
 
 if __name__ == '__main__':
